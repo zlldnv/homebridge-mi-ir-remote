1ae60f06d3929c45356f7484d79e8a0b22eb5c06
Zzm <1013452574@qq.com>
2017-10-15 12:29:44 +0800

    Update

---
+++ Devices/irLearn.js
@@ -0,0 +1,124 @@
+require('./Base');
+
+const inherits = require('util').inherits;
+const miio = require('miio');
+
+var Accessory, PlatformAccessory, Service, Characteristic, UUIDGen;
+irLearn = function(platform, config) {
+    this.init(platform, config);
+    
+    Accessory = platform.Accessory;
+    PlatformAccessory = platform.PlatformAccessory;
+    Service = platform.Service;
+    Characteristic = platform.Characteristic;
+    UUIDGen = platform.UUIDGen;
+    
+    this.device = new miio.Device({
+        address: this.config['ip'],
+        token: this.config['token']
+    });
+    
+    this.accessories = {};
+    this.accessories['RemoteAccessory'] = new irLearnButton(this);
+    var accessoriesArr = this.obj2array(this.accessories);
+    
+    this.platform.log.debug("[MiIRRemote][irLearn][DEBUG]Initializing learn: " + this.config["ip"] + ", accessories size: " + accessoriesArr.length);
+    
+    
+    return accessoriesArr;
+}
+inherits(irLearn, Base);
+
+irLearnButton = function(dThis) {
+    this.device = dThis.device;
+    this.name = "MiLearn";
+    this.token = dThis.config['token'];
+    this.platform = dThis.platform;
+    this.updatetimere = false;
+    this.timer;
+    this.upt;
+    this.irLearnService;
+    this.timekey;
+}
+
+irLearnButton.prototype.getServices = function() {
+    var that = this;
+    var services = [];
+    var tokensan = this.token.substring(this.token.length-8);
+    var infoService = new Service.AccessoryInformation();
+    infoService
+        .setCharacteristic(Characteristic.Manufacturer, "XiaoMi")
+        .setCharacteristic(Characteristic.Model, "ChuangMi IR Remote")
+        .setCharacteristic(Characteristic.SerialNumber, tokensan);
+    services.push(infoService);
+    var irLearnButtonService = this.irLearnService = new Service.Switch(this.name);
+    var irLearnButtonOnCharacteristic = irLearnButtonService.getCharacteristic(Characteristic.On);
+    irLearnButtonOnCharacteristic
+        .on('set',function(value, callback) {
+            this.platform.log.info("[MiIRRemote][irLearn] 6666" + value);
+            if(value == true){
+                this.updatetimere = true;
+                this.upt = 5;
+                this.updateTimer();
+            }            
+            callback(null);
+        }.bind(this))
+        .on('get', function(callback) {
+            callback(null, false);
+        }.bind(this))
+        
+    services.push(irLearnButtonService);
+    return services;
+}
+
+irLearnButton.prototype.updateTimer = function() {
+    if (this.updatetimere) {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(function() {
+            this.runTimer();
+            this.updateTimer();
+        }.bind(this), 1 * 1000);
+    }
+}
+
+irLearnButton.prototype.runTimer = function() {
+    var that = this;
+    this.upt = this.upt - 1;
+    if(this.upt <= 0){
+        this.updatetimere = false;
+        this.irLearnService.getCharacteristic(Characteristic.On).updateValue(false);
+        that.platform.log.info("[MiIRRemote][irLearn] Learn Stopped");
+    }else{
+        this.timekey = "123456789012345";
+        if(this.upt == 4){
+            this.device.call("miIO.ir_learn", {"key":this.timekey}).then(result => {
+                that.platform.log.info("[MiIRRemote][irLearn][DEBUG]irLearn Waiting...");
+            }).catch(function(err) {
+                if(err == "Error: Call to device timed out"){
+                    that.platform.log.debug("[MiIRRemote][ERROR]irLearn - Remote Offline");
+                }else{
+                    that.platform.log.error("[MiIRRemote][irLearn][ERROR] Error: " + err);
+                }
+            });
+        }else{
+            this.device.call("miIO.ir_read", {"key":this.timekey}).then(result => {
+                if(result['code'] !== ""){
+                    that.platform.log.info("[MiIRRemote][irLearn][DEBUG]irLearn " + result['code']);
+                    this.updatetimere = false;
+                    this.upt = 0;
+                    this.irLearnService.getCharacteristic(Characteristic.On).updateValue(false);
+                    that.platform.log.info("[MiIRRemote][irLearn] Learn Success!");
+                }else{
+                    that.platform.log.info("[MiIRRemote][irLearn][DEBUG]Learn Waiting...");
+                }
+            }).catch(function(err) {
+                if(err == "Error: Call to device timed out"){
+                    that.platform.log.debug("[MiIRRemote][ERROR]irLearn - Remote Offline");
+                }else{
+                    that.platform.log.error("[MiIRRemote][irLearn][ERROR] Error: " + err);
+                }
+            });
+        }
+        that.platform.log.info("[MiIRRemote][irLearn] " + this.upt + " Seconds left");
+    }
+}
---
+++ index.js
@@ -0,0 +1,102 @@
+require('./Devices/irLearn');
+require('./Devices/MiSwitch');
+
+var fs = require('fs');
+var packageFile = require("./package.json");
+var PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
+
+module.exports = function(homebridge) {
+    if(!isConfig(homebridge.user.configPath(), "platforms", "ChuangmiIRPlatform")) {
+        return;
+    }
+    
+    PlatformAccessory = homebridge.platformAccessory;
+    Accessory = homebridge.hap.Accessory;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    UUIDGen = homebridge.hap.uuid;
+
+    homebridge.registerPlatform('homebridge-mi-ir-remote', 'ChuangmiIRPlatform', ChuangmiIRPlatform, true);
+}
+
+function isConfig(configFile, type, name) {
+    var config = JSON.parse(fs.readFileSync(configFile));
+    if("accessories" === type) {
+        var accessories = config.accessories;
+        for(var i in accessories) {
+            if(accessories[i]['accessory'] === name) {
+                return true;
+            }
+        }
+    } else if("platforms" === type) {
+        var platforms = config.platforms;
+        for(var i in platforms) {
+            if(platforms[i]['platform'] === name) {
+                return true;
+            }
+        }
+    } else {
+    }
+    
+    return false;
+}
+
+function ChuangmiIRPlatform(log, config, api) {
+    if(null == config) {
+        return;
+    }
+    
+    this.Accessory = Accessory;
+    this.PlatformAccessory = PlatformAccessory;
+    this.Service = Service;
+    this.Characteristic = Characteristic;
+    this.UUIDGen = UUIDGen;
+    
+    this.log = log;
+    this.config = config;
+
+    if (api) {
+        this.api = api;
+    }
+    
+    
+    this.log.info("[MiIRRemote][INFO]*********************************************************************");
+    this.log.info("[MiIRRemote][INFO]                           MiIRRemote v%s                        *",packageFile.version);
+    this.log.info("[MiIRRemote][INFO] GitHub: https://github.com/Zzm317/homebridge-mi-ir-remote          *");
+    this.log.info("[MiIRRemote][INFO]                                                                    *");
+    this.log.info("[MiIRRemote][INFO]*********************************************************************");
+    this.log.info("[MiIRRemote][INFO]start success...");
+    
+}
+
+ChuangmiIRPlatform.prototype = {
+    accessories: function(callback) {
+        var myAccessories = [];
+        if(this.config['hidelearn'] == false){
+            new irLearn(this, this.config['learnconfig']).forEach(function(accessory, index, arr){
+                myAccessories.push(accessory);
+            });
+        }
+        var deviceCfgs = this.config['deviceCfgs'];
+        
+        if(deviceCfgs instanceof Array) {
+            for (var i = 0; i < deviceCfgs.length; i++) {
+                var deviceCfg = deviceCfgs[i];
+                if(null == deviceCfg['type'] || "" == deviceCfg['type'] || null == deviceCfg['token'] || "" == deviceCfg['token'] || null == deviceCfg['ip'] || "" == deviceCfg['ip']) {
+                    continue;
+                }
+                
+                if (deviceCfg['type'] == "Switch") {
+                    new MiSwitch(this, deviceCfg).forEach(function(accessory, index, arr){
+                        myAccessories.push(accessory);
+                    });
+                } else {
+                }
+            }
+            this.log.info("[MiIRRemote][INFO]device size: " + deviceCfgs.length + ", accessories size: " + myAccessories.length);
+        }
+        
+
+        callback(myAccessories);
+    }
+}
